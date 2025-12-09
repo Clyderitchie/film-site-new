47f2523e18ca71877918d29d0bdeaba2916a8031
francis <clyderitchie@yahoo.com>
2025-12-08 22:06:21 -0800

    Tailwindcss installed

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 
-// import "./home.css";
-
 function Home() {
   return (
     <>
-      <div className="container border border-primary w-100 mt-4">
-        <div className="row">
-          <h1>homepage layout</h1>
-        </div>
-      </div>
+      <div className="text-right">Hello world</div>
     </>
   );
 }
